dfd0791e590ede51705ce3636bd44d68c370d744
mary.e.beliveau@gmail.com Beliveau <mary.e.beliveau@gmail.com>
2018-08-19 01:58:42 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,8 +17,9 @@ def long_planeteer_calls(array_of_calls)
 end
 
 def find_the_cheese(array_of_cheese)
-  array_of_cheese.include?(cheese_types)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array_of_cheese.include?(cheese_types)
+  
 end
 
 roll_call_dwarves(['Dopey', 'Sleepy', 'Bashful'])
