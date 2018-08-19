1f22f0fc176b7bb1003367074956b8948dd26179
mary.e.beliveau@gmail.com Beliveau <mary.e.beliveau@gmail.com>
2018-08-19 01:56:32 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,7 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(array_of_calls)
-  array_of_calls.include? do |call|
+  array_of_calls.any? do |call|
     call.length > 4
   end 
 end
