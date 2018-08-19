20b40f76c236a18e82bd747607b18a28d3ad522a
mary.e.beliveau@gmail.com Beliveau <mary.e.beliveau@gmail.com>
2018-08-19 02:08:20 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,29 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarf_names)
+  dwarf_names.each_with_index do |item, index|
+    puts "#{index+1} *#{item}"
+  end 
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.collect do |call|
+    call.capitalize + "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array_of_calls)
+  array_of_calls.any? do |call|
+    call.length > 4
+  end 
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array_of_snacks)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array_of_snacks.each do |snack|
+    cheese_types.each do |cheese|
+      if snack == cheese
+        return snack
+      end 
+    end
+  end
+  return nil 
 end
