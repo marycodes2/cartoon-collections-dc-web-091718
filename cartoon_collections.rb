f231c70f4dee5b177e47f3ba504b63f9fe23e4d3
mary.e.beliveau@gmail.com Beliveau <mary.e.beliveau@gmail.com>
2018-08-19 02:03:32 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,7 +16,7 @@ def long_planeteer_calls(array_of_calls)
   end 
 end
 
-def find_the_cheese(array_of_cheese)
+def find_the_cheese(array_of_snacks)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array_of_cheese.find(cheese_types)
+  array_of_snacks.
 end
